b7c17b40c7dc31519c8270b9b0a0e68e735e93f3
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-02-11 15:30:00 +0100

    date and time test

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ num01 = 5.12345
 num02 = num01.toFixed(2);
 console.log(num02)
 
-// reduce digits
-num02.toPrecision(4)
-console.log(num02)
+// date and time
+let newDate = new Date();
+
+console.log(newDate);
+
 
